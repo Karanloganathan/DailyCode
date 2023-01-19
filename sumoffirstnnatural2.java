55220135f4c9cf1420d39696e4477af445ac208f
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-19 19:34:37 +0530

    Create sumoffirstnnatural2.java

---
+++ sumoffirstnnatural2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class sumoffirstnnatural2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //int sum=n*(n+1)/2;
+       // System.out.println(sum);
+       System.out.println(n*(n+1)/2);
+    }
+}
+
+// Time complexity : O(1)
+// Space complexity : O(1)
