c64c6e5ab423db02ae2ecb5fafb27b37e6966f39
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-23 20:54:36 +0530

    Add files via upload

---
+++ areaofCircle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class areaofCircle {
+    public static void main(String[] args){
+        Scanner ss=new Scanner(System.in);
+    double r,area;
+     r=ss.nextDouble();
+    area=Math.PI*(r*r);
+System.out.printf("%.3f",area);
+ss.close();
+    }
+}
+
+/*Using format() Method of String class
+Using round() method of Math class */
