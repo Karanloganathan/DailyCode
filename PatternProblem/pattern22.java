8dd9e7d1231ddc456442b409d05950c3d6eec1b7
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-02-10 19:22:59 +0530

    Add files via upload

---
+++ PatternProblem/pattern22.java
@@ -0,0 +1,30 @@
+public class pattern22 {
+    public static void main(String[] args){
+       
+        int num=5;
+        pattern(num);
+    }
+
+    static void pattern(int n){
+        for(int row=0;row<n;row++){
+            for(int col=0;col<2*n-1;col++){
+                if((row+col==4)||(col-row==4)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    
+    }
+}
+
+/*
+ OUTPUT
+    *    
+   * *
+  *   *
+ *     *
+*       *
+ */
