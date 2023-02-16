ed0478906db278249adb91fb23d707d739a2e362
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-02-16 06:29:36 +0530

    Create stringsintro.java

---
+++ strings/stringsintro.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Scanner;
+public class stringsintro {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a="kunal";
+        String b="kushwaha";
+        System.out.println(a);
+        System.out.print(b);
+       
+        sc.nextLine();
+
+        String c=sc.nextLine();
+        
+        System.out.println(c);
+
+        String n1=new String("karan");
+        System.out.println(n1);
+    }
+}
